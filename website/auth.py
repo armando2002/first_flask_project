601496347f751af189e46cb3e527008c4f2ea894
Stephen <armando2002@gmail.com>
2022-12-21 23:41:29 -0800

    added boostrap and basic template inheritance

--- website/auth.py
+++ website/auth.py
@@ -1,6 +1,6 @@
 # file used to setup authentication routes
 
-from flask import Blueprint
+from flask import Blueprint, render_template
 # set up an auth blueprint for flask app
 auth = Blueprint('auth', __name__)
 
@@ -8,7 +8,7 @@ auth = Blueprint('auth', __name__)
 # define login
 @auth.route('/login')
 def login():
-    return "<p>Login</>"
+    return render_template("login.html", boolean=False)
 
 
 # define logout
@@ -20,4 +20,4 @@ def logout():
 # define signup
 @auth.route('/signup')
 def signup():
-    return "<p>signup</p>"
+    return render_template("signup.html")
